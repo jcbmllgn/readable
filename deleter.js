e705e08270abb87f1817ab583fba7d2612f667c7
Jacob Mulligan <jacobdmulligan@gmail.com>
2012-08-03 01:04:14 -0400

    First push! HUZZAH

---
+++ deleter.js
@@ -0,0 +1,5 @@
+javascript:(function(){ 
+	$('div#sticky_sharing').remove(); 
+	$('div.top_link_strip').remove();
+	$('div#leaderboardad').remove();
+	$('div.global enable_sticky_header').remove(); })();
